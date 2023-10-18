c60b4c1936e594c45a70cf8a09f432a18ec8fbbb
maria_s_pr@mail.ru <maria_s_pr@mail.ru>
2023-10-19 00:16:48 +0500

    Домашнее задание по Семинару 4

---
+++ Lesson4/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Lesson4
+{
+    //Дан массив и число.Найдите три числа в массиве сумма которых равна искомому числу.
+    //Подсказка: если взять первое число в массиве, 
+     //можно ли найти в оставшейся его части два числа равных по сумме первому.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+
+            int target = 189;
+
+            var s = new HashSet<int>();
+
+            foreach (int j in arr)
+            {
+                foreach (int i in arr)
+                {
+                    var x = target - i - j;
+                    if (s.Contains(x))
+                    {
+                        Console.WriteLine($"{target} = {x} + {i} + {j} ");
+                    }
+                    else
+                    {
+                        s.Add(i);
+                    }
+                }
+            }
+        }
+    }
+}
