25b819a62328f4faeb05ad616e6fbbf88c5d7170
maria_s_pr@mail.ru <maria_s_pr@mail.ru>
2023-11-01 11:57:54 +0500

    Домашнее задание по Семинару 8

---
+++ Lesson8/Lesson8/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Lesson8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 3)
+            {
+                Console.WriteLine("Укажите через пробел имя каталога где искать файл, расширение файла и искомую строку!");
+                return;
+            }
+
+            string searchDirectory = args[0];
+            string fileName = args[1];
+            string str = args[2];
+
+            try
+            {
+                ResearchRecursive(searchDirectory, fileName,str);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Что-то пошло не так: {ex.Message}");
+            }
+        }
+
+        static void ResearchRecursive(string dirNAme, string fileName, string str)
+        {
+            string[] matchingFiles = Directory.GetFiles(dirNAme, "*" +fileName);
+
+            foreach (string file in matchingFiles)
+            {               
+                using (var reader = new StreamReader(File.Open(file, FileMode.Open)))
+                {
+
+
+                    while (reader.Peek() >= 0)
+                    {
+                        var line = reader.ReadLine();
+
+                        if (line!.ToLower().Contains(str.ToLower()))
+                        {
+                            Console.WriteLine($"Найдена строка: {line} в файле {file}" );
+                        }
+                    }
+                }
+            }
+
+            string[] subDirectories = Directory.GetDirectories(dirNAme);
+
+            foreach (string subDirectory in subDirectories)
+            {
+                ResearchRecursive(subDirectory, fileName, str);
+            }
+        }
+
+    }
+}
